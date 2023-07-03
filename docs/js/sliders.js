14ff38d36c973f676c2b56d7f0177d6b80ec3764
alina-onmaster <alina.onmaster@gmail.com>
2023-07-03 16:10:21 +0300

    Переделана главная и верстка части внутренних страниц

--- docs/js/sliders.js
+++ docs/js/sliders.js
@@ -38,3 +38,55 @@ new Swiper('.topical-links__wrap.swiper', {
     }
 });
 
+new Swiper('.exposition__gallery-wrap.swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    breakpoints: {
+        320: {
+            spaceBetween: 20,
+        },
+
+        768: {
+            centeredSlides: true,
+            spaceBetween: 30,
+            slidesPerView: 1.5,
+        },
+
+        1440: {
+            centeredSlides: true,
+            spaceBetween: 30,
+            slidesPerView: 3,
+        },
+    }
+});
+
+new Swiper('.museum-area__gallery-wrap.swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    breakpoints: {
+        320: {
+            spaceBetween: 20,
+        },
+
+        768: {
+            centeredSlides: true,
+            spaceBetween: 30,
+            slidesPerView: 1.5,
+        },
+
+        1440: {
+            centeredSlides: true,
+            spaceBetween: 30,
+            slidesPerView: 3,
+        },
+    }
+});
+
